d46442e54607f84a2b443b393f05ec2d6f4dd64b
Fabrício Barbosa Viegas <incy8498@gmail.com>
2025-02-27 17:08:07 -0300

    Setup inicial

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "cache.h"
+#include "utils.h"
+
+int main(int argc, char *argv[]){
+    if(argc != 7){
+        std::cerr << "Uso: ./cache_simulator <nsets> <bsize> <assoc> <substituição> <flag_saida> arquivo_entrada\n";
+        return 1;
+    }
+    else{
+    }
+    return 0;
+}
